397f0a8585e082d27f1ea831840d27e8c2b24d88
ni4yja <k2.liuba@gmail.com>
2021-11-11 21:08:49 +0200

    BaseInput added

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}</label>
+    <div class="control">
+      <input
+        v-bind="$attrs"
+        :placeholder="label"
+        class="input is-primary"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/ContactForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="section">
+      <div class="card is-horizontal columns">
+        <div class="card-image column is-two-fifths p-0">
+          <figure class="image">
+            <img alt="programming" src="../assets/programming.jpg" />
+          </figure>
+        </div>
+        <div class="card-content column is-three-fifths p-6">
+          <p class="title">Contact us</p>
+          <div class="columns mt-4">
+            <div class="column">
+              <BaseInput 
+                v-model="contact.name"
+                label="Name"
+                type="text"
+              />
+              <BaseInput 
+                v-model="contact.mail"
+                label="Mail"
+                type="email"
+              />
+            <BaseInput 
+                v-model="contact.phone"
+                label="Phone"
+                type="tel"
+              />
+            </div>
+            <div class="column">
+              <div class="field message-field">
+                <label class="label">Message</label>
+                <div class="control">
+                  <textarea
+                    class="textarea is-primary"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Services</label>
+            <div class="control">
+              <label class="radio">
+                <input type="radio" name="answer" checked>
+                Web Design
+              </label>
+              <label class="radio">
+                <input type="radio" name="answer">
+                Web Development
+              </label>
+              <label class="radio">
+                <input type="radio" name="answer">
+                Graphic Design
+              </label>
+              <label class="radio">
+                <input type="radio" name="answer">
+                Logo
+              </label>
+            </div>
+          </div>
+          <button class="button is-primary mt-4">Send Message</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseInput from '../components/BaseInput.vue'
+
+export default {
+  components: {
+    BaseInput
+  },
+  data() {
+    return {
+      contact: {
+        name: '',
+        mail: '',
+        phone: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
