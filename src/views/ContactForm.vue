dbf5b54dd4c32ceae8e535f09191c3d3bd53e11e
ni4yja <k2.liuba@gmail.com>
2021-11-13 13:37:53 +0200

    form submission added

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -8,24 +8,26 @@
           </figure>
         </div>
         <div class="card-content column is-three-fifths p-6">
-          <p class="title">Contact us, please</p>
-          <div class="columns mt-4">
-            <div class="column">
-              <BaseInput v-model="contact.name" label="Name" type="text" />
-              <BaseInput v-model="contact.mail" label="Mail" type="email" />
-              <BaseInput v-model="contact.phone" label="Phone" type="tel" />
+          <p class="title">Contact us</p>
+          <form @submit.prevent="sendForm">
+            <div class="columns mt-4">
+              <div class="column">
+                <BaseInput v-model="contact.name" label="Name" type="text" />
+                <BaseInput v-model="contact.mail" label="Mail" type="email" />
+                <BaseInput v-model="contact.phone" label="Phone" type="tel" />
+              </div>
+              <div class="column">
+                <BaseTextarea v-model="contact.message" label="Message" />
+              </div>
             </div>
-            <div class="column">
-              <BaseTextarea v-model="contact.message" label="Message" />
-            </div>
-          </div>
-          <BaseRadioGroup
-            v-model="contact.services"
-            label="Services"
-            name="services"
-            :options="services"
-          />
-          <button class="button is-primary mt-4">Send Message</button>
+            <BaseRadioGroup
+              v-model="contact.services"
+              label="Services"
+              name="services"
+              :options="services"
+            />
+            <button type="submit" class="button is-primary mt-4">Send Message</button>
+          </form>
         </div>
       </div>
     </div>
@@ -33,8 +35,7 @@
 </template>
 
 <script>
-import BaseRadioGroup from "../components/BaseRadioGroup.vue";
-
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -53,7 +54,20 @@ export default {
       ]
     };
   },
-  components: { BaseRadioGroup },
+  methods: {
+    sendForm () {
+      axios.post(
+        'https://my-json-server.typicode.com/ni4yja/contact-form/contact',
+        this.contact
+      )
+        .then(function (response) {
+          console.log('Response', response)
+        })
+        .catch(function (err) {
+          console.log('Error', err)
+        })
+    }
+  }
 };
 </script>
 
